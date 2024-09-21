736334738e2929aa94c8b28ee9a83853a5a1ba0d
mhavens17 <mhavens@smu.edu>
2024-09-20 20:19:16 -0500

    Initial Commit

---
+++ iOSTravelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSTravelApp
+//
+//  Created by Max Havens on 9/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
