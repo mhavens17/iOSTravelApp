d57d83b462e95d9a7b368727e168600e5df405f5
nico <nicomorin01@gmail.com>
2024-09-22 23:46:04 -0500

    Added Switch

--- iOSTravelApp/ItineraryViewController.swift
+++ iOSTravelApp/ItineraryViewController.swift
@@ -12,7 +12,8 @@ import UIKit
 class ItineraryViewController: UIViewController {
     
     var itinerary: [String]?
-
+    @IBOutlet weak var isPlanning: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,6 +22,16 @@ class ItineraryViewController: UIViewController {
         // Display the itinerary (you can add your own UI elements for this)
         if let itinerary = itinerary {
             print("Itinerary: \(itinerary.joined(separator: "\n"))")
+        }
+    }
+    @IBAction func donePlanning(_ sender: Any) {
+        //        cast sender as switch and set label according to the state
+        if let switchSender = sender as? UISwitch {
+            if switchSender.isOn {
+                isPlanning.text = "Finished Planning"
+            } else {
+                isPlanning.text = "Planning"
+            }
         }
     }
 }
